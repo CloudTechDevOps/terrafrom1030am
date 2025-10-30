ce996acb12ef4ce95e37739a3c51aec3e836633e
veera <veerababu.narni232@gmail.com>
2025-10-30 11:54:45 +0530

    state

--- Day-4-state-locking/backend.tf
+++ Day-4-state-locking/backend.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {
     bucket = "terraform-bucket-state-file-storage"
     key    = "terraform.tfstate"
-    use_lockfile = true # to use s3 native locking 
+    #use_lockfile = true # to use s3 native locking 1.19 version above
     region = "us-east-1"
+    dynamodb_table = "veera" # any version we can use dynamodb locking 
+    encrypt = true
   }
 }
