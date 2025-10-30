6c3ebaa87ab0521f3011542e7dd59fed6ab01182
veera <veerababu.narni232@gmail.com>
2025-10-30 11:41:36 +0530

    day-4-statelock

--- Day-4-state-locking/backend.tf
+++ Day-4-state-locking/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-bucket-state-file-storage"
     key    = "terraform.tfstate"
+    use_lockfile = true # to use s3 native locking 
     region = "us-east-1"
   }
 }
