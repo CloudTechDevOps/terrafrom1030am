653be1613ad01833d31eb05eac23dbc92a65a106
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-11-18 15:22:17 +0530

    Delete Day-13-root-child-modules directory

--- Day-13-root-child-modules/modules/ec2/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_2_id
-
-  tags = {
-    Name = "WebServer"
-  }
-}
--- Day-13-root-child-modules/modules/rds/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "subnet_1_Id" {}
-variable "subnet_1_id" {}
-variable "instance_class" {}
-variable "db_name" {}
-variable "db_user" {}
-variable "db_password" {}
